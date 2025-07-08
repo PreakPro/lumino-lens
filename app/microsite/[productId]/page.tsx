2a2bbc0ebe0ecb7f89c7d2c84c5b200dccacbfd9
PreakPro <shaurya.preak1239@outlook.com>
2025-07-09 00:43:08 +0530

    props type error fix

--- app/microsite/[productId]/page.tsx
+++ app/microsite/[productId]/page.tsx
@@ -1,19 +1,17 @@
 type Props = {
- 	 	params: { productId: string }
+    params: { productId: string }
 }
 
 export default function MicrositePage({ params }: Props) {
-  	const id = params.productId
-  	const pdfUrl = `/api/certificate/${id}`
+    const id = params.productId
+    const pdfUrl = `/api/certificate/${id}`
 
-	if(!id) return <h1 className="md:text-5xl text-3xl justify-center items-center flex">Loading...</h1>
-
-  	return (
-  	  	<iframe
-  	    	src={pdfUrl}
-  	    	style={{ border: "none" }}
-			className="h-screen w-full"
-  	    	title={`Certificate for ${id}`}
-  	  	/>
-  	)
+    return (
+        <iframe
+            src={pdfUrl}
+            style={{ border: "none" }}
+            className="h-screen w-full"
+            title={`Certificate for ${id}`}
+        />
+    )
 }
