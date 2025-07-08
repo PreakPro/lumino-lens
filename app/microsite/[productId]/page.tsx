63da22b42ab36f6113d888e4977538637804b7f9
PreakPro <shaurya.preak1239@outlook.com>
2025-07-09 01:06:07 +0530

    props type error fix attempt 4

--- app/microsite/[productId]/page.tsx
+++ app/microsite/[productId]/page.tsx
@@ -1,17 +1,26 @@
-export default function MicrositePage({
-    params,
-}: {
-    params: { productId: string };
-}) {
-    const id = params.productId;
-    const pdfUrl = `/api/certificate/${id}`;
+"use client"
+
+import { useParams } from "next/navigation";
+
+export default function MicrositePage() {
+    const { productId } = useParams() as { productId: string };
+
+    if (!productId) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-3xl md:text-5xl font-semibold">Loading...</h1>
+            </div>
+        );
+    }
+
+    const pdfUrl = `/api/certificate/${productId}`;
 
     return (
         <iframe
             src={pdfUrl}
             style={{ border: "none" }}
             className="h-screen w-full"
-            title={`Certificate for ${id}`}
+            title={`Certificate for ${productId}`}
         />
     );
 }
